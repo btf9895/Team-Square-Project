2d87426242a5cc8fa1238a733baa17a031182402
FullVolumeNoise <41074743+FullVolumeNoise@users.noreply.github.com>
2023-12-03 23:16:40 -0500

    Moved parseSaveFile to its own class.

--- src/main/java/EdgeConvertFileParser.java
+++ src/main/java/EdgeConvertFileParser.java
@@ -198,73 +198,7 @@ public class EdgeConvertFileParser {
       } // connectors for() loop
    } // resolveConnectors()
    
-   public void parseSaveFile() throws IOException { //this method is unclear and confusing in places
-      logger.info("Parsing Save file...");
-      StringTokenizer stTables, stNatFields, stRelFields, stNatRelFields, stField;
-      EdgeTable tempTable;
-      EdgeField tempField;
-      currentLine = br.readLine();
-      currentLine = br.readLine(); //this should be "Table: "
-      while (currentLine.startsWith("Table: ")) {
-
-         numFigure = Integer.parseInt(currentLine.substring(currentLine.indexOf(" ") + 1)); //get the Table number
-         currentLine = br.readLine(); //this should be "{"
-         currentLine = br.readLine(); //this should be "TableName"
-         tableName = currentLine.substring(currentLine.indexOf(" ") + 1);
-         tempTable = new EdgeTable(numFigure + DELIM + tableName);
-         
-         currentLine = br.readLine(); //this should be the NativeFields list
-         stNatFields = new StringTokenizer(currentLine.substring(currentLine.indexOf(" ") + 1), DELIM);
-         numFields = stNatFields.countTokens();
-         for (int i = 0; i < numFields; i++) {
-            tempTable.addNativeField(Integer.parseInt(stNatFields.nextToken()));
-         }
-         
-         currentLine = br.readLine(); //this should be the RelatedTables list
-         stTables = new StringTokenizer(currentLine.substring(currentLine.indexOf(" ") + 1), DELIM);
-         numTables = stTables.countTokens();
-         for (int i = 0; i < numTables; i++) {
-            tempTable.addRelatedTable(Integer.parseInt(stTables.nextToken()));
-         }
-         tempTable.makeArrays();
-         
-         currentLine = br.readLine(); //this should be the RelatedFields list
-         stRelFields = new StringTokenizer(currentLine.substring(currentLine.indexOf(" ") + 1), DELIM);
-         numFields = stRelFields.countTokens();
-
-         for (int i = 0; i < numFields; i++) {
-            tempTable.setRelatedField(i, Integer.parseInt(stRelFields.nextToken()));
-         }
-
-         alTables.add(tempTable);
-         currentLine = br.readLine(); //this should be "}"
-         currentLine = br.readLine(); //this should be "\n"
-         currentLine = br.readLine(); //this should be either the next "Table: ", #Fields#
-      }
-      while ((currentLine = br.readLine()) != null) {
-       //
-         stField = new StringTokenizer(currentLine, DELIM);
-         
-         // numFigure = Integer.parseInt(stField.nextToken());
-         numFigure = Integer.parseInt( stField.nextToken().substring(11) );
-         fieldName = stField.nextToken().substring(5) ;
-         tempField = new EdgeField(numFigure + DELIM + fieldName);
-        
-         tempField.setTableID(Integer.parseInt(stField.nextToken().substring(8).trim() ));
-         tempField.setTableBound(Integer.parseInt( stField.nextToken().substring(11).trim() ));
-        
-         tempField.setFieldBound(Integer.parseInt( stField.nextToken().substring(11).trim() ));
-         tempField.setDataType(Integer.parseInt( stField.nextToken().substring(9).trim() ));
-         tempField.setVarcharValue( Integer.parseInt(stField.nextToken().substring(14).trim() ));
-         tempField.setIsPrimaryKey(Boolean.valueOf(stField.nextToken()).booleanValue());
-         tempField.setDisallowNull(Boolean.valueOf(stField.nextToken()).booleanValue());
-         if (stField.hasMoreTokens()) { //Default Value may not be defined
-            tempField.setDefaultValue( stField.nextToken().substring(14) );
-         }
-         alFields.add(tempField);
-      }
-      logger.debug("Finished parsing Save file.");
-   } // parseSaveFile()
+   
 
    private void makeArrays() { //convert ArrayList objects into arrays of the appropriate Class type
       if (alTables != null) {
@@ -310,7 +244,11 @@ public class EdgeConvertFileParser {
             this.resolveConnectors(); //Identify nature of Connector endpoints
          } else {
             if (currentLine.startsWith(SAVE_ID)) { //the file chosen is a Save file created by this application
-               this.parseSaveFile(); //parse the file
+
+               ParseSaveFile p = new ParseSaveFile(currentLine, numFigure, tableName, numFields, numTables, fieldName, alFields, alTables, br, parseFile, fr); 
+               p.parseSaveFile();
+               //parseSaveFile(); //parse the file
+
                br.close();
                this.makeArrays(); //convert ArrayList objects into arrays of the appropriate Class type
             } else { //the file chosen is something else
